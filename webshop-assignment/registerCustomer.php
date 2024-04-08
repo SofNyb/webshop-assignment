db263abd2ab5f546b1ae6de101263b9ae67846da
SofNyb <sofi13a5@edu.zealand.dk>
2024-04-08 17:05:14 +0200

    Rydde op i ubrugt kode

--- webshop-assignment/registerCustomer.php
+++ webshop-assignment/registerCustomer.php
@@ -45,18 +45,6 @@ if (isset($_POST['userName']) && isset($_POST['userEmail']) && isset($_POST['use
     }
 endif;
 ?>
-<!--
-<p>Hej</p>
-
-<form action="registerCustomer.php" method="post">
-    <label>Brugernavn:</label><br>
-    <input type="text" name="userName"><br>
-    <label>E-mailadresse:</label><br>
-    <input type="email" name="userEmail"><br>
-    <label>Adgangskode:</label><br>
-    <input type="password" name="userPW"><br>
-    <input type="submit" value="Opret">
-</form>-->
 
 <div class="container mt-5 text-center">
     <div class="row">
