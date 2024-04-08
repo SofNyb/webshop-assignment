0c296eb9227e2aad9f5231fec2a782ce6cc3ddef
SofNyb <sofi13a5@edu.zealand.dk>
2024-04-08 14:25:00 +0200

    Includet header+footer-file, Start-container til produkter

--- webshop-assignment/products.php
+++ webshop-assignment/products.php
@@ -1,3 +1,19 @@
 <?php
 
+require_once "db.php";
+include "includes/header.php";
 
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <div>Hej</div>
+        </div>
+    </div>
+</div>
+
+
+<?php
+
+include "includes/footer.php";
