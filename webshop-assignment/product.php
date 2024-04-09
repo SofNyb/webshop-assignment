44b6d302dca651d03a66e54ad3b8ceeefb1e20c6
SofNyb <sofi13a5@edu.zealand.dk>
2024-04-09 17:27:50 +0200

    Product- og products-filer opdateret, og taler nu sammen

--- webshop-assignment/product.php
+++ webshop-assignment/product.php
@@ -3,17 +3,81 @@ session_start();
 require_once "db.php";
 include "includes/head.php";
 
-$sql = 'SELECT * FROM product';
+/*
+$productID = $_GET["productID"];
+$products = $handler->sql("SELECT * FROM product WHERE productID=$productID")
+*/
 
-try {
-    // Forbered og udfør forespørgslen
-    $stmt = $handler->query($sql);
+if(isset($_GET["productID"])) {
+    $productID = $_GET["productID"];
 
-    // Hent alle rækker som et associativt array
-    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    // Håndtering af databasefejl
-    echo "Error: " . $e->getMessage();
+    /*$sql = "SELECT * FROM product WHERE productID = '$productID'";
+    $stmt = $handler->prepare($sql);
+    $stmt->execute();
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);*/
+
+    try {
+        $sql = "SELECT * FROM product WHERE productID = '$productID'";
+        $stmt = $handler->prepare($sql);
+        $stmt->execute();
+        $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch(PDOException $e) {
+        echo "Fejl: " . $e->getMessage();
+    }
+
 }
 
-?>
+if(isset($products) && !empty($products)) {
+    foreach ($products as $product) { ?>
+
+    <div class="container mt-5">
+
+        <div class="row">
+
+            <div class="col">
+                <div class="card">
+                    <!--<img src="uploads/<?php /*echo $product['productPicture']; */?>" class="card-img-top" alt="<?php /*echo $product['productName']; */?>">-->
+                </div>
+
+                <div class="card">
+
+                    <div class="card-body">
+
+                        <div class="row">
+
+                            <div class="col">
+                                <h5 class="card-title">
+                                    <?php echo $product['productName']; ?>
+                                </h5>
+                            </div>
+
+                        </div>
+
+                        <p> - <?php echo $product['productType']; ?></p>
+                        <p class="card-text">
+                            <?php echo $product['productBrand']; ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $product['productColor']; ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo $product['productType']; ?>
+                        </p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+<?php }
+}else {
+    echo "Fejl";
+}
+
+include "includes/footer.php";
--- webshop-assignment/products.php
+++ webshop-assignment/products.php
@@ -3,7 +3,7 @@ session_start();
 require_once "db.php";
 include "includes/head.php";
 
-$sql = 'SELECT * FROM product';
+$sql = 'SELECT productID, productName, productPicture, productPrice FROM product';
 
 try {
     // Forbered og udfør forespørgslen
@@ -15,7 +15,7 @@ try {
     // Håndtering af databasefejl
     echo "Error: " . $e->getMessage();
 }
-
+/*<a href="product.php?prodID=<?php /*echo $product['productID'] ?>">*/
 ?>
 
 <div class="container mt-5 text-center">
@@ -28,33 +28,43 @@ try {
 
 <div class="container mt-5">
     <div class="row">
-            <?php foreach ($products as $product) : ?>
-        <div class="col-sm-6 col-md-4">
-            <div class="card mb-3">
-                <div class="card-body">
-                        <!--<img src="<?php /*echo $product['productPicture']; */?>" class="card-img-top" alt="<?php /*echo $product['productName']; */?>">-->
-                    <div class="row">
-                        <div class="col">
-                            <h5 class="card-title">
-                                <?php echo $product['productName']; ?>
-                            </h5>
-                        </div>
-                    </div>
-                    <p> - <?php echo $product['productType']; ?></p>
-                    <p class="card-text">
-                        <?php echo $product['productBrand']; ?>
-                    </p>
-                    <p class="card-text">
-                        <?php echo $product['productColor']; ?>
-                    </p>
-                    <p class="card-text">
-                        <?php echo $product['productType']; ?>
-                    </p>
-                </div>
-            </div>
 
-        </div>
+        <div class="col-sm-6 col-md-4">
+            <?php foreach ($products as $product) : ?>
+
+                <div class="card mb-3">
+                    <div class="card-body">
+                            <!--<img src="<?php /*echo $product['productPicture']; */?>" class="card-img-top" alt="<?php /*echo $product['productName']; */?>">-->
+                        <div class="row">
+                            <div class="col">
+                                <h5 class="card-title">
+                                    <?php echo $product['productName']; ?>
+                                </h5>
+                            </div>
+                        </div>
+                        <!--<p> - <?php /*echo $product['productType']; */?></p>
+                        <p class="card-text">
+                            <?php /*echo $product['productBrand']; */?>
+                        </p>
+                        <p class="card-text">
+                            <?php /*echo $product['productColor']; */?>
+                        </p>
+                        <p class="card-text">
+                            <?php /*echo $product['productType']; */?>
+                        </p>-->
+                    </div>
+                <p>
+                    <a href="product.php?productID=<?php echo $product['productID'] ?>">
+                        Læs mere
+                    </a>
+                </p>
+                </div>
+
+
+            <!--</a>-->
             <?php endforeach; ?>
+        </div>
+
     </div>
 </div>
 
