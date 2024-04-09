027f45a571e2a4700d505dac0b5045b0a4cdc399
SofNyb <sofi13a5@edu.zealand.dk>
2024-04-09 20:24:29 +0200

    Tilføjelse af opsætning af produkt-card

--- webshop-assignment/product.php
+++ webshop-assignment/product.php
@@ -20,48 +20,73 @@ if(isset($_GET["productID"])) {
 if(isset($products) && !empty($products)) {
     foreach ($products as $product) { ?>
 
+        <div class="container mt-5 text-center">
+            <div class="row">
+                <div class="col">
+                    <h1><?php echo $product['productName']; ?> - <?php echo $product['productBrand']; ?></h1>
+                </div>
+            </div>
+        </div>
+
     <div class="container mt-5">
 
-        <div class="row">
+        <div class="card mb-3">
+            <div class="row g-0">
 
-            <div class="col">
-                <div class="card">
-                    <!--<img src="uploads/<?php /*echo $product['productPicture']; */?>" class="card-img-top" alt="<?php /*echo $product['productName']; */?>">-->
+                <div class="col-md-4">
+                    <img src="uploads/<?php echo $product['productPicture']; ?>" class="card-img-top" alt="<?php echo $product['productName']; ?>">
                 </div>
 
-                <div class="card">
-
+                <div class="col-md-8">
                     <div class="card-body">
-
-                        <div class="row">
-
-                            <div class="col">
-                                <h5 class="card-title">
-                                    <?php echo $product['productName']; ?>
-                                </h5>
-                            </div>
-
-                        </div>
-
-                        <p> - <?php echo $product['productType']; ?></p>
-                        <p class="card-text">
-                            <?php echo $product['productBrand']; ?>
+                        <h4 class="card-title">
+                            <?php echo $product['productName']; ?>
+                        </h4>
+                        <p class="card-text text-muted">
+                            <?php echo $product['productDesc']; ?>
                         </p>
                         <p class="card-text">
+                            <strong class="text-muted">Farve:</strong>
                             <?php echo $product['productColor']; ?>
                         </p>
-                        <p class="card-text">
-                            <?php echo $product['productType']; ?>
-                        </p>
+
+                        <h5 class="card-text">
+                            <?php echo $product['productPrice']; ?>kr.
+                        </h5>
+
+                        <div class="form-group mt-3">
+                            <form action="product.php?productID=<?php echo $_GET["productID"]; ?>" method="post">
+                                <input type="number" name="data[productAmount]" value="1" min="1" placeholder="Antal" required>
+                                <input type="hidden" name="data[productName]" value="<?php echo $product['productName']; ?>">
+                                <input type="hidden" name="data[productPicture]" value="<?php echo $product['productPicture']; ?>">
+                                <input type="hidden" name="data[productPrice]" value="<?php echo $product['productPrice']; ?>">
+
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Tilføj til kurv</button>
+
+                                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Produkt tilføjet til kurv</h1>
+                                                    <button type="submit" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="submit" class="btn text-light btn-success" data-bs-dismiss="modal">
+                                                        Fortsæt
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                            </form>
+                        </div>
 
                     </div>
-
                 </div>
 
             </div>
-
         </div>
-
     </div>
 
 
