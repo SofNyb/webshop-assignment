67e9ad45b541accdfabb9e06178a1c21cf655e8f
SofNyb <sofi13a5@edu.zealand.dk>
2024-04-09 15:39:19 +0200

    Opdateret navbar

--- webshop-assignment/includes/navbar.php
+++ webshop-assignment/includes/navbar.php
@@ -1,13 +1,6 @@
 <?php
 
-/*// Start session:
-session_start();*/
-
 require_once "db.php";
-/*require_once "login.php";*/
-
-/*//hvis brugeren er logget ind, kan deres rolle hentes fra sessionen:
-$userRole = isset($_SESSION['userRole']) ? $_SESSION['userRole'] : null;*/
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -19,13 +12,7 @@ $userRole = isset($_SESSION['userRole']) ? $_SESSION['userRole'] : null;*/
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
                 <a class="nav-link active" aria-current="page" href="/products.php">Produkter</a>
-
-                <?php
-                /*                //tjek for at se, om bruger er administrator:
-                                    if($userRole == 1) {
-                                        echo '<br><a class="nav-link" href="/adminRegister.php">Tilføj produkt</a>';
-                                    }
-                                */?>
+                <a class="nav-link active" aria-current="page" href="/profile.php">Profil</a>
                 <a class="nav-link" href="#">Log ud</a>
             </div>
         </div>
