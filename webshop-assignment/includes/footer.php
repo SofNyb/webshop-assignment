59947f8025b47003092503dbbd39fd2ac5c36034
SofNyb <sofi13a5@edu.zealand.dk>
2024-04-11 12:02:46 +0200

    Ændret webshop-navn

--- webshop-assignment/includes/footer.php
+++ webshop-assignment/includes/footer.php
@@ -1,3 +1,9 @@
+<nav class="navbar fixed-bottom navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Heart and Home</a>
+    </div>
+</nav>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
--- webshop-assignment/includes/navbar.php
+++ webshop-assignment/includes/navbar.php
@@ -5,7 +5,7 @@ require_once "db.php";
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Webshop</a>
+        <a class="navbar-brand" href="index.php">Heart and Home</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
