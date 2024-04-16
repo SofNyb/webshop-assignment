e5bc486827810446de47b695db530b70d628e485
SofNyb <sofi13a5@edu.zealand.dk>
2024-04-16 11:48:32 +0200

    Tilføjet favicon

--- webshop-assignment/includes/head.php
+++ webshop-assignment/includes/head.php
@@ -8,6 +8,7 @@
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
+    <link rel="icon" type="image/x-icon" href="/images/heart.png">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/bc87d7ac19.js" crossorigin="anonymous"></script>
